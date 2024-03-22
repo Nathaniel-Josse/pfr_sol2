784ce5926d421299fef54eee5664698c92934725
Nathaniel <nathaniel.josse@gmail.com>
2024-03-22 22:59:24 +0100

    Initial setup

---
+++ frontend/src/components/header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
+const Header = () => {
+
+    return(
+        <>
+            <header>
+                <nav>
+                    <ul>
+                        <li><Link to='home'>Accueil</Link></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <section>
+                <Outlet />
+            </section>
+        </>
+    )
+}
+export default Header;

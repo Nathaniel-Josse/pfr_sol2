b8df41873541bedab6c79266d25530e7bbf56ab5
Nathaniel <nathaniel.josse@gmail.com>
2024-03-31 15:57:29 +0200

    backend correction

---
+++ frontend/constants/api.js
@@ -0,0 +1,5 @@
+export const URL = {
+    USER_SIGNUP: 'http://localhost:8000/api/user/signup',
+    USER_GET: 'http://localhost:8000/api/user/getusers',
+    FILM_LIST: 'http://localhost:8000/api/film/getfilms',
+}

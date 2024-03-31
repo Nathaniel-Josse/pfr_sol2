8a3cf388d39b0a717d68f0227e70b4a99720826b
Nathaniel <nathaniel.josse@gmail.com>
2024-03-31 15:12:25 +0200

    Revert "backend structure updated + frontend minor preparations"
    This reverts commit ebfc4cccc19ef5a3acb2bb4c58a6335bf66e5ace.

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,39 +1,10 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 
 export default function Home() {
-
-    const [data, setData] = useState(null);
-    
-    const fetchData = async () => {
-        const response = await axios.get("http://localhost:2024/getFilms");
-        setData(response.data);
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-
-
     
     return (
         <div>
         <h1>Home</h1>
-
-        <h2>Test avec la BDD. Voici les données des films :</h2>
-
-        {data && data.map((film) => (
-            <div key={film.id}>
-                <h3>{film.titre}</h3>
-                <p>{film.realisateur}</p>
-                <p>{film.annee}</p>
-                <p>{film.nationalite}</p>
-                <p>{film.duree}</p>
-                <p>{film.genre}</p>
-                <p>{film.synopsis}</p>
-            </div>
-        ))}
         </div>
     );
 }

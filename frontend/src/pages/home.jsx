1baba2acf15e9388876369d431eb6d37c9cfbb55
Nathaniel <nathaniel.josse@gmail.com>
2024-03-31 16:28:40 +0200

    axios implementation started

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { URL } from "../constants/api";
 
 export default function Home() {
+
+    const getFilms = async () => {
+        try {
+            const response = await axios.get(URL.FILM_LIST);
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getFilms();
+    }, []);
     
+
+
     return (
         <div>
         <h1>Home</h1>
